3c5f45e0ca28e9fe9dcfbae215bebba4bdb93581
ganaa <ganaa@users.noreply.github.com>
2018-10-07 22:32:17 +0200

    script.js converted to es6

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
-    $('.nav-button').click(function() {
-        $('.nav-button').toggleClass('change');
-    });
-    $(window).scroll(function() {
-        let position = $(this).scrollTop();
-        if(position >= 200) {
-            $('.nav-menu').addClass('custom-navbar');
-        } else {
-            $('.nav-menu').removeClass('custom-navbar');
-        }
-    });
+$(document).ready(() => {
+  $('.nav-button').click(() => {
+    $('.nav-button').toggleClass('change');
+  });
+  $(window).scroll(function() {
+    const position = $(this).scrollTop();
+    if (position >= 200) {
+      $('.nav-menu').addClass('custom-navbar');
+    } else {
+      $('.nav-menu').removeClass('custom-navbar');
+    }
+  });
 });
