7c054c1da711ab74d8318bff74c60e8440e70805
ganaa <ganaa@users.noreply.github.com>
2018-10-13 00:53:38 +0200

    bootstrap project3 completed

--- script.js
+++ script.js
@@ -10,4 +10,49 @@ $(document).ready(() => {
       $('.nav-menu').removeClass('custom-navbar');
     }
   });
+
+  $(window).scroll(() => {
+    const position = $(this).scrollTop();
+    if (position >= 650) {
+      $('.camera-img').addClass('fromLeft');
+      $('.mission-text').addClass('fromRight');
+    } else {
+      $('.camera-img').removeClass('fromLeft');
+      $('.mission-text').removeClass('fromRight');
+    }
+  });
+
+  $('.gallery-list-item').click(function() {
+    const value = $(this).attr('data-filter');
+    if (value === 'all') {
+      $('.filter').show(300);
+    } else {
+      $('.filter')
+        .not(`.${value}`)
+        .hide(300);
+      $('.filter')
+        .filter(`.${value}`)
+        .show(300);
+    }
+  });
+
+  $('.gallery-list-item').click(function() {
+    $(this)
+      .addClass('active-item')
+      .siblings()
+      .removeClass('active-item');
+  });
+
+  $(window).scroll(() => {
+    const position = $(this).scrollTop();
+    if (position >= 4300) {
+      $('.card-1').addClass('moveFromLeft');
+      $('.card-2').addClass('moveFromBottom');
+      $('.card-3').addClass('moveFromRight');
+    } else {
+      $('.card-1').removeClass('moveFromLeft');
+      $('.card-2').removeClass('moveFromBottom');
+      $('.card-3').removeClass('moveFromRight');
+    }
+  });
 });
